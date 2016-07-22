0ab6852c0cfd40fc72d2d8ccff164c92f8616542
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-07-22 10:08:02 +0200

    coverage shows up

--- ObservableList/test/conftest.py
+++ ObservableList/test/conftest.py
@@ -4,7 +4,7 @@ import sys
 import os
 
 HERE = os.path.dirname(__file__)
-sys.path.append(os.path.join(HERE, "..", ".."))
+sys.path.insert(0, os.path.join(HERE, "..", ".."))
 
 if sys.version_info[0] == 2:
     import mock
