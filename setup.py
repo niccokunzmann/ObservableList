0f77252400866146da39df7be13c325a3190c211
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-07-22 09:20:01 +0200

    added more pzthon versions

--- setup.py
+++ setup.py
@@ -252,7 +252,10 @@ SETUPTOOLS_METADATA = dict(
         'Intended Audience :: Manufacturing',
         'Natural Language :: English',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         development_state
         ],
     package_data=dict(
